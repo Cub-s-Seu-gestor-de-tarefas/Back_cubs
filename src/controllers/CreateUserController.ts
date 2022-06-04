fecc6517c6dfa757358b39a7abd39c60c10531ba
Kaique Eduardo Lemes da Silva Cardoso <kaiquecardoso.kec@gmail.com>
2022-06-03 23:24:59 -0300

    img bixos random and the img bixos are been sent by getMember

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -21,13 +21,24 @@ class CreateUserController {
         const passwordHash = await hash(password, 8);
 
         const userEmail = await prismaClient.user.findFirst({ where: { email: email }, select: { email: true } });
-       
-       
+
+
         if (userEmail != null) {
             throw new Error("Este email ja esta cadastrado!");
         }
+        const bixos = ["aguia", "bixo_preguica", "bode", "castor", "cavalo", "cervo", "coruja",
+            "crocodilo", "elefante", "girafa", "gorila", "guaxinim", "hipopotamo", "lama", "leao",
+            "lobo", "macaco", "meerkat", "panda", "pinguim", "raposa", "tigre", "touro", "urso",
+            "zebra"];
+
+            const bName = bixos[Math.floor(Math.random() * bixos.length)];
+
+         let bImg = bName + ".png";
+         console.log(bImg);
+
+
         const user = await prismaClient.user.create({
-            data: { name: name, password: passwordHash, email: email,img:""  }
+            data: { name: name, password: passwordHash, email: email, img: bImg }
         });
 
         return response.json(user);
--- src/socketControllers/SocketUsers.ts
+++ src/socketControllers/SocketUsers.ts
@@ -2,7 +2,7 @@ import { prismaClient } from "@database/prismaClient";
 class SocketUsers {
     async getMembers(text: string) {
         if (text != "" && text != null) {
-            const Emails = await prismaClient.user.findMany({ where: { email: { startsWith: text } }, select: { email: true } });
+            const Emails = await prismaClient.user.findMany({ where: { email: { startsWith: text } }, select: { email: true,img:true } });
             return Emails;
         }
 
