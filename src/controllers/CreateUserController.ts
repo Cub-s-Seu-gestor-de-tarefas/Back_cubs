f283544a9b423d0cf5e5e31479021d266fa2dcff
H3llder <eusouhelder123@gmail.com>
2022-10-25 18:50:47 -0300

    Revert ".fix border-radiusicons and searchResponsived"
    This reverts commit ff5e2a04ff0df11b924f9769672daec48f5fdc68.

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -11,21 +11,21 @@ class CreateUserController {
         if (!email) {
             throw new Error("Email incorrect");
         }
-        // console.log("Password", password);
+        console.log("Password", password);
 
 
         if (!password) {
-            throw new Error("Password is empty");
+            throw new Error("Password is empity");
         }
 
         const passwordHash = await hash(password, 8);
 
         const userEmail = await prismaClient.user.findFirst({ where: { email: email }, select: { email: true } });
 
+
         if (userEmail != null) {
             throw new Error("Este email ja esta cadastrado!");
         }
-
         const bixos = ["aguia", "bicho_preguica", "bode", "castor", "cavalo", "cervo", "coruja",
             "crocodilo", "elefante", "girafa", "gorila", "guaxinim", "hipopotamo", "lama", "leao",
             "lobo", "macaco", "meerkat", "panda", "pinguim", "raposa", "tigre", "touro", "urso",
